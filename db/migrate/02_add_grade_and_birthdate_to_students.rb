79d3a059c3360fc131b07a0b5281029f4694b0fe
Gracie McGuire <m.graciemcguire@gmail.com>
2018-12-03 22:09:57 +0000

    Automatically backed up by Learn

--- db/migrate/02_add_grade_and_birthdate_to_students.rb
+++ db/migrate/02_add_grade_and_birthdate_to_students.rb
@@ -1,5 +1,6 @@
-class CreateStudents < ActiveRecord::Migration[5.1]
+class CreateSt  students < ActiveRecord::Migration[5.1]
   def change 
     add_column :students, :grade, :integer
     add_column :students, :birthdate, :string
   end
+endrolling_in_the_deep.artist
